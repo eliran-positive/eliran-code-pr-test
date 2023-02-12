81d5e2628a37fa59398c2f366049b4b44e413c85
Eliran Arkabi <eliran@valu.co.il>
2023-02-12 13:23:46 +0200

    Fix eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,6 @@ module.exports = {
     "eslint:recommended",
     "plugin:vue/vue3-essential",
     "plugin:@typescript-eslint/recommended",
-    // "plugin:css/recommended"
   ],
   "overrides": [
   ],
@@ -20,21 +19,14 @@ module.exports = {
   "plugins": [
     "vue",
     "@typescript-eslint",
-    // "css",
-    // "scss"
   ],
   "rules": {
     "brace-style": ["error", "1tbs"],
     "curly": ["error", "multi-line"],
     "keyword-spacing": ["error", { "before": true }],
     "space-before-blocks": ["error", "always"],
-    "indent": ["error", 2],// 4 or 2?
+    "indent": ["error", 2],
     "semi": ["error", "always"],
     "space-before-function-paren": ["error", "always"],
-
-    // scss
-    // "scss/at-extend-no-missing-placeholder": 2,
-    // "scss/at-function-pattern": [2, { "name": "^[a-z]+([a-z0-9-]+[a-z0-9]+)?$" }],
-    // "scss/at-import-no-partial-leading-underscore": 2,
   }
 };
